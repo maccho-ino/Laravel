17530b014a9910a1c7715f2067147e253730b6af
Ayaka Ino <maccho0723@gmail.com>
2022-04-20 09:52:41 +0000

    PHP/Laravel 09

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+/*「http://XXXXXX.jp/XXX というアクセスが来たときに、 
+AAAControllerのbbbというAction に渡すRoutingの設定」を
+書いてみてください*/
+
+Route::get('xxx', 'AAAController@bbb');
+
+/*admin/profile/create にアクセスしたら
+ProfileController の add Action に、
+admin/profile/edit にアクセスしたら
+ProfileController の edit Action に
+割り当てるように設定してください*/
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create','ProfileController@add');
+    Route::get('profile/edit','ProfileController@edit');
+});
